8c32c3777d3f9666c25f812633b44bcfb0361232
BoJulien <julien.bonnet@ynov.com>
2025-02-24 16:08:41 +0100

    enable mode paysage

--- cinemate/App.js
+++ cinemate/App.js
@@ -1,15 +1,28 @@
-// App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from './src/context/ThemeContext';
 import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'react-native';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function App() {
+  useEffect(() => {
+    // Permettre toutes les orientations (portrait et paysage)
+    async function enableAllOrientations() {
+      await ScreenOrientation.unlockAsync();
+    }
+    
+    enableAllOrientations();
+    
+    // Nettoyer lors du démontage de l'application
+    return () => {
+      ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+    };
+  }, []);
+
   return (
     <ThemeProvider>
+      <StatusBar translucent backgroundColor="transparent" />
       <AppNavigator />
     </ThemeProvider>
   );
 }
-
-
-
